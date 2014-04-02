1b0726fff8e849e24292deaec5289fb83bfd6f1d
Dare Ryan <dareryan@me.com>
2014-04-02 10:28:21 -0400

    add segue for test auth screen

---
+++ ShortPath/ShortPath/VisitorsViewController.h
@@ -0,0 +1,13 @@
+//
+//  VisitorsViewController.h
+//  ShortPath
+//
+//  Created by Dare Ryan on 4/2/14.
+//  Copyright (c) 2014 Eugene Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VisitorsViewController : UIViewController
+
+@end

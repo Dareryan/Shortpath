e7e464df0c41c99be1674654939e31964436830a
Nadia Yudina <joystate@me.com>
2014-04-11 16:24:25 -0400

    pedicate works

--- ShortPath/ShortPath/VisitorsVC.m
+++ ShortPath/ShortPath/VisitorsVC.m
@@ -47,6 +47,12 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     
+    self.searchBar.delegate = self;
+    self.searchDisplayController.delegate = self;
+    self.searchDisplayController.searchResultsDataSource = self;
+    self.searchDisplayController.searchResultsDelegate = self;
+    
+    
     self.dataStore = [ShortPathDataStore sharedDataStore];
     
     NSFetchRequest *req = [[NSFetchRequest alloc]initWithEntityName:@"Visitor"];
@@ -122,8 +128,9 @@
     if (tableView == self.searchDisplayController.searchResultsTableView) {
         
         return [self.searchResults count];
-    }
-    else {
+        
+    } else {
+        
         return [[self.sections valueForKey:[[[self.sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:section]] count];
         
     }
@@ -179,6 +186,8 @@
 
 -(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
 {
+    NSLog(@"%@", searchString);
+    
     [self filterContentForSearchText:searchString
                                scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
                                       objectAtIndex:[self.searchDisplayController.searchBar
@@ -188,185 +197,6 @@
 }
 
 
-
-//- (void)updateFilteredContentForSearchString:(NSString *)searchString productType:(NSString *)type
-//{
-//    // start out with the entire list
-//    self.searchResults = [self.visitors mutableCopy];
-//    
-//    // strip out all the leading and trailing spaces
-//    NSString *strippedStr = [searchString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-//    
-//    // break up the search terms (separated by spaces)
-//    NSArray *searchItems = nil;
-//    
-//    if (strippedStr.length > 0)
-//    {
-//        searchItems = [strippedStr componentsSeparatedByString:@" "];
-//    }
-//    
-//    // build all the "AND" expressions for each value in the searchString
-//    //
-//    NSMutableArray *andMatchPredicates = [NSMutableArray array];
-//    
-//    for (NSString *searchString in searchItems)
-//    {
-//        // each searchString creates an OR predicate for: name, yearIntroduced, introPrice
-//        //
-//        // example if searchItems contains "iphone 599 2007":
-//        //      name CONTAINS[c] "iphone"
-//        //      name CONTAINS[c] "599", yearIntroduced ==[c] 599, introPrice ==[c] 599
-//        //      name CONTAINS[c] "2007", yearIntroduced ==[c] 2007, introPrice ==[c] 2007
-//        //
-//        NSMutableArray *searchItemsPredicate = [NSMutableArray array];
-//        
-//        // name field matching
-//        NSExpression *lhs = [NSExpression expressionForKeyPath:@"name"];
-//        NSExpression *rhs = [NSExpression expressionForConstantValue:searchString];
-//        NSPredicate *finalPredicate = [NSComparisonPredicate
-//                                       predicateWithLeftExpression:lhs
-//                                       rightExpression:rhs
-//                                       modifier:NSDirectPredicateModifier
-//                                       type:NSContainsPredicateOperatorType
-//                                       options:NSCaseInsensitivePredicateOption];
-//        [searchItemsPredicate addObject:finalPredicate];
-//        
-//        // at this OR predicate to our master AND predicate
-//        NSCompoundPredicate *orMatchPredicates = (NSCompoundPredicate *)[NSCompoundPredicate orPredicateWithSubpredicates:searchItemsPredicate];
-//        [andMatchPredicates addObject:orMatchPredicates];
-//    }
-//    
-//    NSCompoundPredicate *finalCompoundPredicate = nil;
-//    
-//    if (type != nil)
-//    {
-//        // we have a scope type to narrow our search further
-//        //
-//        if (andMatchPredicates.count > 0)
-//        {
-//            // we have a scope type and other fields to search on -
-//            // so match up the fields of the Product object AND its product type
-//            //
-//            NSCompoundPredicate *compPredicate1 =
-//            (NSCompoundPredicate *)[NSCompoundPredicate andPredicateWithSubpredicates:andMatchPredicates];
-//            NSPredicate *compPredicate2 = [NSPredicate predicateWithFormat:@"(SELF.type == %@)", type];
-//            
-//            finalCompoundPredicate =
-//            (NSCompoundPredicate *)[NSCompoundPredicate andPredicateWithSubpredicates:@[compPredicate1, compPredicate2]];
-//        }
-//        else
-//        {
-//            // match up by product scope type only
-//            finalCompoundPredicate =
-//            (NSCompoundPredicate *)[NSPredicate predicateWithFormat:@"(SELF.type == %@)", type];
-//        }
-//    }
-//    else
-//    {
-//        // no scope type specified, just match up the fields of the Product object
-//        finalCompoundPredicate =
-//        (NSCompoundPredicate *)[NSCompoundPredicate andPredicateWithSubpredicates:andMatchPredicates];
-//    }
-//    
-//    self.searchResults = [[self.searchResults filteredArrayUsingPredicate:finalCompoundPredicate] mutableCopy];
-//}
-//
-//#pragma mark - State restoration
-//
-//// Note:
-//// UITableView itself supports current scroll position and selected row if its data source
-//// supports UIDataSourceModeAssociation protocol.
-////
-//// To make things simpler here, we choose to manually preserve/restore the table view selection
-//// and not support scroll position.
-//
-//static NSString *SearchDisplayControllerIsActiveKey = @"SearchDisplayControllerIsActiveKey";
-//static NSString *SearchBarScopeIndexKey = @"SearchBarScopeIndexKey";
-//static NSString *SearchBarTextKey = @"SearchBarTextKey";
-//static NSString *SearchBarIsFirstResponderKey = @"SearchBarIsFirstResponderKey";
-//
-//static NSString *SearchDisplayControllerSelectedRowKey = @"SearchDisplayControllerSelectedRowKey";
-//static NSString *TableViewSelectedRowKey = @"TableViewSelectedRowKey";
-//
-//
-//- (void)encodeRestorableStateWithCoder:(NSCoder *)coder
-//{
-//    [super encodeRestorableStateWithCoder:coder];
-//    
-//    UISearchDisplayController *searchDisplayController = self.searchDisplayController;
-//    
-//    BOOL searchDisplayControllerIsActive = [searchDisplayController isActive];
-//    [coder encodeBool:searchDisplayControllerIsActive forKey:SearchDisplayControllerIsActiveKey];
-//    
-//    if (searchDisplayControllerIsActive)
-//    {
-//        [coder encodeObject:[searchDisplayController.searchBar text] forKey:SearchBarTextKey];
-//        [coder encodeInteger:[searchDisplayController.searchBar selectedScopeButtonIndex] forKey:SearchBarScopeIndexKey];
-//        
-//        NSIndexPath *selectedIndexPath = [searchDisplayController.searchResultsTableView indexPathForSelectedRow];
-//        if (selectedIndexPath != nil)
-//        {
-//            [coder encodeObject:selectedIndexPath forKey:SearchDisplayControllerSelectedRowKey];
-//        }
-//        
-//        BOOL searchFieldIsFirstResponder = [searchDisplayController.searchBar isFirstResponder];
-//        [coder encodeBool:searchFieldIsFirstResponder forKey:SearchBarIsFirstResponderKey];
-//    }
-//    
-//    NSIndexPath *selectedIndexPath = [self.tableView indexPathForSelectedRow];
-//    if (selectedIndexPath != nil)
-//    {
-//        [coder encodeObject:selectedIndexPath forKey:TableViewSelectedRowKey];
-//    }
-//}
-//
-//- (void)decodeRestorableStateWithCoder:(NSCoder *)coder
-//{
-//    [super decodeRestorableStateWithCoder:coder];
-//    
-//    BOOL searchDisplayControllerIsActive = [coder decodeBoolForKey:SearchDisplayControllerIsActiveKey];
-//    
-//    if (searchDisplayControllerIsActive)
-//    {
-//        [self.searchDisplayController setActive:YES];
-//        
-//        // order is important here. Setting the search bar text causes
-//        // searchDisplayController:shouldReloadTableForSearchString: to be invoked.
-//        //
-//        NSInteger searchBarScopeIndex = [coder decodeIntegerForKey:SearchBarScopeIndexKey];
-//        [self.searchDisplayController.searchBar setSelectedScopeButtonIndex:searchBarScopeIndex];
-//        
-//        NSString *searchBarText = [coder decodeObjectForKey:SearchBarTextKey];
-//        if (searchBarText != nil)
-//        {
-//            [self.searchDisplayController.searchBar setText:searchBarText];
-//        }
-//        
-//        NSIndexPath *selectedIndexPath = [coder decodeObjectForKey:SearchDisplayControllerSelectedRowKey];
-//        if (selectedIndexPath != nil)
-//        {
-//            [self.searchDisplayController.searchResultsTableView selectRowAtIndexPath:selectedIndexPath animated:NO scrollPosition:UITableViewScrollPositionTop];
-//        }
-//        
-//        BOOL searchFieldIsFirstResponder = [coder decodeBoolForKey:SearchBarIsFirstResponderKey];
-//        if (searchFieldIsFirstResponder)
-//        {
-//            [self.searchDisplayController.searchBar becomeFirstResponder];
-//        }
-//    }
-//    
-//    NSIndexPath *selectedIndexPath = [coder decodeObjectForKey:TableViewSelectedRowKey];
-//    if (selectedIndexPath != nil)
-//    {
-//        [self.tableView selectRowAtIndexPath:selectedIndexPath animated:NO scrollPosition:UITableViewScrollPositionTop];
-//    }
-//}
-
-//- (IBAction)addNewVisitor:(id)sender {
-//    UIActionSheet *newVisitorAS = [[UIActionSheet alloc] initWithTitle:@"Assign to" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"New Event", @"Existing Event", nil];
-//    
-//    [newVisitorAS showInView:self.view];
-//}
 
 #pragma mark - map sections to visitors
 

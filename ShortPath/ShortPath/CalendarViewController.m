90b655d3475a358f29099c4bc6e510ba27ee55c1
bramplex <eeronmo@gmail.com>
2014-04-16 14:30:18 -0400

    Added progress indicator

--- ShortPath/ShortPath/CalendarViewController.m
+++ ShortPath/ShortPath/CalendarViewController.m
@@ -14,6 +14,7 @@
 #import "Visitor+Methods.h"
 #import "APIClient.h"
 #import "User+Methods.h"
+#import "SVProgressHUD.h"
 
 @interface CalendarViewController ()
 
@@ -39,11 +40,15 @@
 {
     [super viewDidLoad];
     
+    [SVProgressHUD show];
+    
     self.dataStore = [ShortPathDataStore sharedDataStore];
     
     self.apiClient = [[APIClient alloc]init];
     
     //NSLog(@"Time test: %@", [Event timeStringFromDate:[[NSDate alloc]init]]);
+    
+    [SVProgressHUD dismiss];
 
 }
 

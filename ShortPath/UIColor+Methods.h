345e9db6f9f9c5993418ccb6a90eda7af5282b08
bramplex <eeronmo@gmail.com>
2014-04-22 14:04:17 -0400

    Added theming

---
+++ ShortPath/UIColor+Methods.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Methods.h
+//  ShortPath
+//
+//  Created by Bram Vandevelde on 2014-04-22.
+//  Copyright (c) 2014 Eugene Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Methods)
+
++ (UIColor *)shortpathBlue;
+
+@end
---
+++ ShortPath/UIColor+Methods.m
@@ -0,0 +1,19 @@
+//
+//  UIColor+Methods.m
+//  ShortPath
+//
+//  Created by Bram Vandevelde on 2014-04-22.
+//  Copyright (c) 2014 Eugene Watson. All rights reserved.
+//
+
+#import "UIColor+Methods.h"
+
+@implementation UIColor (Methods)
+
++ (UIColor *)shortpathBlue
+{
+    UIColor *shortpathBlue = [UIColor colorWithRed:0.024 green:0.439 blue:0.659 alpha:1];
+    return shortpathBlue;
+}
+
+@end
